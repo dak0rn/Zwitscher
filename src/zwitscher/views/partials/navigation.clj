297545dfa777867132ba1d93b52887a0d7a5f0ad
Daniel Koch <daniel@suitsoft.eu>
2017-02-01 11:33:42 +0100

    Fix the navigation code

--- src/zwitscher/views/partials/navigation.clj
+++ src/zwitscher/views/partials/navigation.clj
@@ -1,10 +1,8 @@
 ;; navigation.clj - navigation views for the application
-(ns gw.web.views.partials.navigation)
+(ns zwitscher.views.partials.navigation)
 
 (def links [
-            { :url "/dashboard" :title "Dashboard" }
-            { :url "/reports" :title "Reports" }
-            { :url "/settings" :title "Settings" }
+            { :url "/" :title "Home" }
             ])
 
 (defn
@@ -12,19 +10,15 @@
   "Returns the navigation for the recruiter"
   { :added "0.1.0" }
   []
-  [ :nav.navbar
-   [ :section.navbar-section.top-nav
-    [ :a.navbar-brand { :href "/dashboard" } "github-weekly" ]
-    [ :label.navbar-toggle { :for "toggle-state" }
-     [ :span ]
-     [ :span ]
-     [ :span ]
+  [:nav.nav
+   [:div.nav-left
+    [:a.nav-item.zwitscher {:href "/"} "Zwitscher"]
     ]
-    [ :input#toggle-state { :type "checkbox" } ]
-    [ :div.navbar-contents
-     (for [ link links ]
-       [ :a.btn.btn-link { :href (:url link) } (:title link) ])
-     ]
+
+   [:span.nav-toggle [:span] [:span] [:span]]
+
+   [:div.nav-right.nav-menu
+    [:nav-item]
     ]
    ]
   )
