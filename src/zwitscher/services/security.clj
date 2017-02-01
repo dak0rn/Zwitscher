352787cef01316b8eaadcf031aafc3261681da49
Daniel Koch <daniel@suitsoft.eu>
2017-02-01 19:19:42 +0100

    Add token management integrated with redis

--- src/zwitscher/services/security.clj
+++ src/zwitscher/services/security.clj
@@ -2,7 +2,9 @@
 (ns zwitscher.services.security
   (:require [buddy.sign.jwt :as jwt]
             [buddy.hashers :as hashers]
+            [zwitscher.services.redis :as redis]
             [clj-time.core :as time]
+            [clj-time.coerce :as time-coerce]
             [zwitscher.config :refer [config]]))
 
 (defn
@@ -19,6 +21,13 @@
   [ plain encrypted ]
   (hashers/check plain encrypted))
 
+(defn-
+  get-jwt-ttl
+  "Returns the JWT TTL as clj-time"
+  {:added "0.1.0"}
+  []
+  (get-in config [:security :jwt :ttl]))
+
 (defn
   create-jwt
   "Creates a jwt from the given optional claims. They are merged with the default
@@ -30,12 +39,12 @@
 
   ;; w/ additional claims
   ([ claims ]
-   (let [ jwt-config (get-in config [:security :jwt])
-          ttl (:ttl jwt-config)
-          ;; Expiration timestamp
-          exp (time/plus (time/now) (time/seconds ttl))
-          ;; Merge claims with default claims from the config
-          all-claims (merge claims { :exp exp :rem ttl } (:claims jwt-config)) ]
+   (let [jwt-config (get-in config [:security :jwt])
+         ttl (get-jwt-ttl)
+         ;; Expiration timestamp
+         exp (time-coerce/to-long (time/plus (time/now) (time/seconds ttl)))
+         ;; Merge claims with default claims from the config
+         all-claims (merge claims { :exp exp :rem ttl } (:claims jwt-config)) ]
      (jwt/sign all-claims (:key jwt-config)))))
 
 (defn
@@ -66,3 +75,41 @@
     (let [ header-name (get-in config [:security :cookie-name]) ]
       (assoc-in response [ :cookies header-name ] { :value "" :http-only true :max-age -1 }))))
 
+
+(defn
+  issue-token!
+  "Issues a token for the given user id. The token will be stored in
+   redis"
+  {:added "0.1.0"}
+  [userid]
+  (let [token (create-jwt {:jti userid})]
+    (redis/set token token :ex (get-jwt-ttl))
+    token))
+
+(defn
+  reissue-token!
+  "Revokes the given token and issues a new one for the given user id"
+  {:added "0.1.0"}
+  [token userid]
+  (redis/del token)
+  (issue-token! userid))
+
+(defn
+  token-valid?
+  "Determines if the given token is valid, that is, if it is (still)
+   stored in redis"
+  {:added "0.1.0"}
+  [token]
+  (if (or (nil? token) (empty? token))
+    false
+    (-> token
+        redis/get
+        nil?
+        not)))
+
+(defn
+  invalidate-token!
+  "Invalides the given token by removing it from redis"
+  {:added "0.1.0"}
+  [token]
+  (redis/del token))
