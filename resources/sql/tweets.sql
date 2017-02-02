3f9dce3782fb9d7ec15e6d1ac076d02157c38d81
Daniel Koch <daniel@suitsoft.eu>
2017-02-02 11:16:14 +0100

    Update the SQL
    - Now also returning own tweets
    - Query that inserts a new tweet

--- resources/sql/tweets.sql
+++ resources/sql/tweets.sql
@@ -1,19 +1,39 @@
 -- :name query-tweets-for-user :? :*
-SELECT
-    zw_follows.*,
-    zw_tweet.*,
-    zw_user.*,
-    CASE
-        WHEN zw_likes.ts IS NULL
-        THEN FALSE
-        ELSE TRUE
-    END
-FROM zw_follows
-INNER JOIN zw_user
-    ON zw_user.iduser = zw_follows.whom
-INNER JOIN zw_tweet
-    ON zw_tweet.user_id = zw_user.iduser
-LEFT OUTER JOIN zw_likes
-    ON zw_likes.user_id = zw_user.iduser AND
-       zw_likes.tweet_id = zw_tweet.idtweet
-WHERE zw_follows.who = :uid;
+WITH affected_tweets AS (
+    SELECT
+        zw_user.name,
+        zw_tweet.text,
+        zw_tweet.ts,
+        zw_tweet.idtweet,
+        CASE
+            WHEN zw_likes.ts IS NULL
+            THEN FALSE
+            ELSE TRUE
+        END AS liked
+    FROM zw_follows
+    INNER JOIN zw_user
+        ON zw_user.iduser = zw_follows.whom
+    INNER JOIN zw_tweet
+        ON zw_tweet.user_id = zw_user.iduser
+    LEFT OUTER JOIN zw_likes
+        ON zw_likes.user_id = zw_user.iduser AND
+           zw_likes.tweet_id = zw_tweet.idtweet
+    WHERE zw_follows.who = :uid
+
+    UNION
+
+    SELECT
+        zw_user.name,
+        zw_tweet.text,
+        zw_tweet.ts,
+        zw_tweet.idtweet,
+        NULL as liked
+    FROM zw_tweet
+    INNER JOIN zw_user
+        ON zw_user.iduser = zw_tweet.user_id
+    WHERE zw_tweet.user_id = :uid
+)
+SELECT * FROM affected_tweets ORDER BY ts DESC;
+
+-- :name query-insert-tweet :i!
+INSERT INTO zw_tweet (text, user_id) VALUES (:text, :uid);
