89f40197843533fabe3b25100c3e9711c77d0a7e
Daniel Koch <daniel@suitsoft.eu>
2017-02-02 11:22:58 +0100

    Finish the like button

--- src/zwitscher/views/dashboard.clj
+++ src/zwitscher/views/dashboard.clj
@@ -23,7 +23,8 @@
   "Renders a given tweet"
   {:added "0.1.0"}
   [tweet]
-  (let [name (-> tweet :name h)]
+  (let [name (-> tweet :name h)
+        liked (:liked tweet)]
     [:div.box.tweet-box
      [:p.title.is-5
       [:a {:href (str "/" name)} (str "@" name)]
@@ -33,11 +34,24 @@
      [:p (-> tweet :text h)]
 
      [:div.box-actions
-      (form-to ["POST" "/like"]
-               (anti-forgery-field)
-               [:input {:type "hidden" :value (:idtweet tweet) :name "tweet"}]
-               [:button.button.is-link
-                [:span.fa.fa-heart-o]])
+      (cond
+        (= false liked) (form-to ["POST" "/like"]
+                                 (anti-forgery-field)
+                                 [:input {:type "hidden" :value (:idtweet tweet) :name "tweet"}]
+                                 [:button.button.is-link
+                                  [:span.fa.fa-heart-o]])
+        (= true liked) (form-to ["POST" "/dislike"]
+                                 (anti-forgery-field)
+                                 [:input {:type "hidden" :value (:idtweet tweet) :name "tweet"}]
+                                 [:button.button.is-link
+                                  [:span.fa.fa-heart]])
+
+        :else (form-to ["POST" "/delete"]
+                       (anti-forgery-field)
+                       [:input {:type "hidden" :value (:idtweet tweet) :name "tweet"}]
+                       [:button.button.is-link
+                        [:span.fa.fa-trash-o]])
+        )
       ]
      ]))
 
@@ -56,6 +70,7 @@
                         (anti-forgery-field)
                         [:textarea.textarea {:placeholder "Say something..."
                                              :required true
+                                             :name "text"
                                              :id "say-box"}]
                         [:span#count ]
                         [:button.button.is-primary.pull-right {:id "submit"}
