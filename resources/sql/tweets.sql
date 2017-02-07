23fb729c9b120e50b70e7cbff1a505a9c3662595
Daniel Koch <daniel@suitsoft.eu>
2017-02-07 15:04:59 +0100

    Fix the detection of liked tweets

--- resources/sql/tweets.sql
+++ resources/sql/tweets.sql
@@ -16,8 +16,8 @@ WITH affected_tweets AS (
         ON zw_user.iduser = zw_follows.whom
     INNER JOIN zw_tweet
         ON zw_tweet.user_id = zw_user.iduser
-    LEFT OUTER JOIN zw_likes
-        ON zw_likes.user_id = zw_user.iduser AND
+    LEFT JOIN zw_likes
+        ON zw_likes.user_id = :uid AND
            zw_likes.tweet_id = zw_tweet.idtweet
     WHERE zw_follows.who = :uid
 
